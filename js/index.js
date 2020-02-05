758e177f73e48078f5dc7e8c5c3c95573aa81c25
Source of 0 <SourceOf0@gmail.com>
2020-02-05 17:50:56 +0900

    amend cancel animation

--- js/index.js
+++ js/index.js
@@ -3,13 +3,14 @@ var frameTime = 1 / 30;
 var totalFrames = 1120;
 var frameNumber = 0;
 var canvas = null;
+var requestAnimationIDs = [];
 
 var request = new XMLHttpRequest();
 request.addEventListener("load", function(event) {
   if(request.readyState != 4) return;
   if(request.status != 200 && request.status != 0) return;
   
-  var buffer = request.response;
+  let buffer = request.response;
   pathContainer = PathCtr.initFromBin(buffer);
   console.log("loading completed");
   //console.log(pathContainer);
@@ -23,18 +24,18 @@ window.addEventListener("load", function() {
   
   if(!canvas.parentNode) return;
   
-  var context = canvas.getContext("2d");
+  let context = canvas.getContext("2d");
   if(!context) return;
   
-  var requestAnimationFrame = window.requestAnimationFrame ||
+  let requestAnimationFrame = window.requestAnimationFrame ||
                               window.mozRequestAnimationFrame ||
                               window.webkitRequestAnimationFrame ||
                               window.msRequestAnimationFrame;
-  var cancelAnimationFrame = window.cancelAnimationFrame ||
+  let cancelAnimationFrame = window.cancelAnimationFrame ||
                               window.mozCancelAnimationFrame;
   
-  var viewWidth = document.documentElement.clientWidth;
-  var viewHeight = document.documentElement.clientHeight;
+  let viewWidth = document.documentElement.clientWidth;
+  let viewHeight = document.documentElement.clientHeight;
   
   canvas.setAttribute("style", "position:fixed;z-index:-1;left:0;top:0;width:" + viewWidth + "px;height:" + viewHeight + "px;");
   canvas.width = viewWidth;
@@ -48,28 +49,38 @@ window.addEventListener("load", function() {
     canvas.height = viewHeight;
   });
   
-  var prevTimestamp = 0;
+  let cancelAnimationFrames =()=>{
+    requestAnimationIDs.forEach(id=>{
+      cancelAnimationFrame(id);
+    });
+    requestAnimationIDs.length = 0;
+  };
+  
+  let prevTimestamp = 0;
   (function draw(timestamp) {
     if(!canvas.parentNode) {
-      return cancelAnimationFrame(draw);
+      cancelAnimationFrames();
+      return;
     }
     
-    var elapsed = (timestamp - prevTimestamp) / 1000;
+    let elapsed = (timestamp - prevTimestamp) / 1000;
     //console.log(elapsed, frameTime);
     
     if(!pathContainer || elapsed <= frameTime) {
-      requestAnimationFrame(draw);
+      cancelAnimationFrames();
+      requestAnimationIDs.push(requestAnimationFrame(draw));
       return;
     }
     prevTimestamp = timestamp;
+    
+    cancelAnimationFrames();
+    requestAnimationIDs.push(requestAnimationFrame(draw));
     
     pathContainer.context = context;
     context.clearRect(0, 0, viewWidth, viewHeight);
     pathContainer.setFitSize(viewWidth, viewHeight);
     pathContainer.draw(frameNumber);
     frameNumber = (frameNumber + 1) % totalFrames;
-    
-    requestAnimationFrame(draw);
   })();
 });
 
