8b6bb973bcbe681029d91dc9f89fb9be13f14881
Source of 0 <SourceOf0@gmail.com>
2020-02-15 15:30:22 +0900

    amend comp bat

--- js/_comp.bat
+++ js/_comp.bat
@@ -1,1 +1,13 @@
-type org\*.js > path_control.js
+
+set OUTPUT_FILE=path_control.js
+
+del %OUTPUT_FILE%
+
+type org\PathCtr.js >> %OUTPUT_FILE%
+type org\Matrix.js >> %OUTPUT_FILE%
+type org\Sprite.js >> %OUTPUT_FILE%
+type org\PathObj.js >> %OUTPUT_FILE%
+type org\GroupObj.js >> %OUTPUT_FILE%
+type org\PathContainer.js >> %OUTPUT_FILE%
+type org\PathFactory.js >> %OUTPUT_FILE%
+
