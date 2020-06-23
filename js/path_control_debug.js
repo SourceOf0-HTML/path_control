cc2f5cc1f404a3ac3ad5c100c92593e0fb4da7a8
Source of 0 <SourceOf0@gmail.com>
2020-06-23 13:35:03 +0900

    change scope of draw function

--- js/path_control_debug.js
+++ js/path_control_debug.js
@@ -22,91 +22,42 @@ var PathCtr = {
   binDataPosRange: 20000, // correction value of coordinates when saving to binary data
   
   pathContainer: null,
-  frameTime: 1 / 24,
-  fixFrameTime: 1 / 24,
-  totalFrames: 260,
-  frameNumber: 0,
-  canvas: null,
-  subCanvas: null,
   context: null,
   subContext: null,
-  requestAnimationIDs: [],
-  setTimeoutIDs: [],
   viewWidth: 0,
   viewHeight: 0,
-  prevTimestamp: 0,
-  average: 0,
+  
+  requestAnimationIDs: [],
+  setTimeoutIDs: [],
   
   cancelFunctions: function() {
-    if(PathCtr.requestAnimationIDs.length > 1 || PathCtr.setTimeoutIDs.length > 1) console.log(PathCtr.requestAnimationIDs.length, setTimeoutIDs.length);
-    PathCtr.requestAnimationIDs.forEach(window.cancelAnimationFrame);
-    PathCtr.requestAnimationIDs.length = 0;
-    PathCtr.setTimeoutIDs.forEach(window.clearTimeout);
-    PathCtr.setTimeoutIDs.length = 0;
-  },
-  
-  update: function()  {
-    PathCtr.cancelFunctions();
-    PathCtr.requestAnimationIDs.push(window.requestAnimationFrame(PathCtr.draw));
-    PathCtr.setTimeoutIDs.push(window.setTimeout(PathCtr.update, PathCtr.fixFrameTime*1000));
-  },
-  
-  draw: function(timestamp) {
-    if(!PathCtr.canvas.parentNode) {
-      PathCtr.cancelFunctions();
-      return;
+    if(this.requestAnimationIDs.length > 1 || this.setTimeoutIDs.length > 1) {
+      console.log("requestAnimationIDs:" + this.requestAnimationIDs.length + ", " + setTimeoutIDs.length);
     }
-    
-    if(typeof(timestamp) == "undefined") return;
-    
-    let elapsed = (timestamp - PathCtr.prevTimestamp) / 1000;
-    //console.log(elapsed, PathCtr.average, PathCtr.fixFrameTime);
-    PathCtr.average = (PathCtr.average + elapsed) / 2;
-    PathCtr.prevTimestamp = timestamp;
-    
-    if(!PathCtr.pathContainer) return;
-    
-    PathCtr.canvas.width = PathCtr.subCanvas.width = PathCtr.viewWidth;
-    PathCtr.canvas.height = PathCtr.subCanvas.height = PathCtr.viewHeight;
-    
-    PathCtr.subContext.clearRect(0, 0, PathCtr.viewWidth, PathCtr.viewHeight);
-    PathCtr.pathContainer.draw(PathCtr.frameNumber);
-    PathCtr.frameNumber = (PathCtr.frameNumber + 1) % PathCtr.totalFrames;
-    
-    PathCtr.context.clearRect(0, 0, PathCtr.viewWidth, PathCtr.viewHeight);
-    let imagedata = PathCtr.subContext.getImageData(0, 0, PathCtr.viewWidth, PathCtr.viewHeight);
-    PathCtr.context.putImageData(imagedata, 0, 0);
-    imagedata = null;
-    
-    if(PathCtr.average > PathCtr.frameTime * 2) {
-      PathCtr.fixFrameTime *= 0.99;
-      PathCtr.debugPrint("up");
-    } else if(PathCtr.average < PathCtr.frameTime * 0.5) {
-      PathCtr.fixFrameTime *= 1.01;
-      PathCtr.debugPrint("down");
-    } else {
-      PathCtr.fixFrameTime = (PathCtr.frameTime + PathCtr.fixFrameTime) / 2;
-    }
+    this.requestAnimationIDs.forEach(window.cancelAnimationFrame);
+    this.requestAnimationIDs.length = 0;
+    this.setTimeoutIDs.forEach(window.clearTimeout);
+    this.setTimeoutIDs.length = 0;
   },
   
   init: function() {
-    let container = document.getElementById(PathCtr.defaultCanvasContainerID);
+    let container = document.getElementById(this.defaultCanvasContainerID);
     if(!container) {
       console.error("CanvasContainer is not found.");
       return;
     }
     
-    this.canvas = document.createElement("canvas");
-    this.canvas.className = "main-canvas";
-    container.appendChild(this.canvas);
+    let canvas = document.createElement("canvas");
+    canvas.className = "main-canvas";
+    container.appendChild(canvas);
     
-    this.subCanvas = document.createElement("canvas");
-    this.subCanvas.className = "sub-canvas";
-    this.subCanvas.style.cssText = "display:none;";
-    container.appendChild(this.subCanvas);
+    let subCanvas = document.createElement("canvas");
+    subCanvas.className = "sub-canvas";
+    subCanvas.style.cssText = "display:none;";
+    container.appendChild(subCanvas);
     
-    this.context = this.canvas.getContext("2d");
-    this.subContext = this.subCanvas.getContext("2d");
+    this.context = canvas.getContext("2d");
+    this.subContext = subCanvas.getContext("2d");
     if(!this.context || !this.subContext) {
       console.error("context is not found.");
       return;
@@ -122,20 +73,71 @@ var PathCtr = {
     this.viewWidth = document.documentElement.clientWidth;
     this.viewHeight = document.documentElement.clientHeight;
     
-    this.canvas.setAttribute("style", "position:fixed;z-index:-1;left:0;top:0;width:" + this.viewWidth + "px;height:" + this.viewHeight + "px;");
-    this.canvas.width = this.subCanvas.width = this.viewWidth;
-    this.canvas.height = this.subCanvas.height = this.viewHeight;
+    canvas.setAttribute("style", "position:fixed;z-index:-1;left:0;top:0;width:" + this.viewWidth + "px;height:" + this.viewHeight + "px;");
+    canvas.width = subCanvas.width = this.viewWidth;
+    canvas.height = subCanvas.height = this.viewHeight;
     
-    window.addEventListener("resize", function() {
+    window.addEventListener("resize", ()=> {
       this.viewWidth = document.documentElement.clientWidth;
       this.viewHeight = document.documentElement.clientHeight;
-      this.canvas.setAttribute("style", "position:fixed;z-index:-1;left:0;top:0;width:" + this.viewWidth + "px;height:" + this.viewHeight + "px;");
+      canvas.setAttribute("style", "position:fixed;z-index:-1;left:0;top:0;width:" + this.viewWidth + "px;height:" + this.viewHeight + "px;");
       if(!!this.pathContainer) this.pathContainer.setSize(this.viewWidth, this.viewHeight);
-      //this.update();
     });
     
-    //console.log("base : ", this.frameTime, this.frameTime * 10, this.frameTime * 0.1);
-    this.setTimeoutIDs.push(window.setTimeout(this.update, this.fixFrameTime*1000));
+    
+    let frameTime = 1 / 24;
+    let fixFrameTime = frameTime;
+    let totalFrames = 260;
+    let frameNumber = 0;
+    let prevTimestamp = 0;
+    let average = 0;
+    
+    let draw =(timestamp)=> {
+      if(!canvas.parentNode) {
+        this.cancelFunctions();
+        return;
+      }
+      
+      if(typeof(timestamp) == "undefined") return;
+      
+      let elapsed = (timestamp - prevTimestamp) / 1000;
+      //this.debugPrint(elapsed, average, fixFrameTime);
+      average = (average + elapsed) / 2;
+      prevTimestamp = timestamp;
+      
+      if(!this.pathContainer) return;
+      
+      canvas.width = subCanvas.width = this.viewWidth;
+      canvas.height = subCanvas.height = this.viewHeight;
+      
+      this.subContext.clearRect(0, 0, this.viewWidth, this.viewHeight);
+      this.pathContainer.draw(frameNumber);
+      frameNumber = (frameNumber + 1) % totalFrames;
+      
+      this.context.clearRect(0, 0, this.viewWidth, this.viewHeight);
+      let imagedata = this.subContext.getImageData(0, 0, this.viewWidth, this.viewHeight);
+      this.context.putImageData(imagedata, 0, 0);
+      imagedata = null;
+      
+      if(average > frameTime * 2) {
+        fixFrameTime *= 0.99;
+        this.debugPrint("up");
+      } else if(average < frameTime * 0.5) {
+        fixFrameTime *= 1.01;
+        this.debugPrint("down");
+      } else {
+        fixFrameTime = (frameTime + fixFrameTime) / 2;
+      }
+    };
+    
+    let update =()=> {
+      this.cancelFunctions();
+      this.requestAnimationIDs.push(window.requestAnimationFrame(draw));
+      this.setTimeoutIDs.push(window.setTimeout(update, fixFrameTime*1000));
+    };
+    
+    //this.debugPrint("base : ", frameTime, frameTime * 10, frameTime * 0.1);
+    this.setTimeoutIDs.push(window.setTimeout(update, fixFrameTime*1000));
   },
 };
 
